c5ccee351561a69cf074a2061f14fdacc61e282d
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-15 15:35:15 +0100

    add smaller conan project

---
+++ cmake-project-conan-small/src/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================
+ *      Project :  project-name
+ *      File    :  main.cpp
+ *      Created :  15.12.2021
+ *      Author  :  Dmitry Ivanov
+ * =====================================================================
+ */
+
+#include "main.h"
+
+#include <functional>
+#include <iostream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+int main(int argc, char* argv[], char* env[])
+ {
+    // To turn off messages about unused variables.
+    ((void)argc );
+    ((void)argv );
+    ((void)env );
+
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
+
+    std::cout << "Hello World!" << std::endl;
+
+    // std::system("pause");
+    return 0;
+ }
