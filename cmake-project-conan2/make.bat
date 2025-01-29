7a2d621765362006cc24a8a497047bc91ec681d3
Dmitry Ivanov <d.k.ivanov@live.com>
2025-01-29 19:07:05 +0100

    add conan profiles

--- cmake-project-conan2/make.bat
+++ cmake-project-conan2/make.bat
@@ -1,5 +1,5 @@
-conan install . --output-folder=build --build=missing --settings=build_type=Debug
-conan install . --output-folder=build --build=missing --settings=build_type=Release
-conan install . --output-folder=build --build=missing --settings=build_type=RelWithDebInfo
+conan install . -pr:a=conan\windows-msvc-17-shared-debug-x64.ini          --output-folder=build --build=missing
+conan install . -pr:a=conan\windows-msvc-17-shared-release-x64.ini        --output-folder=build --build=missing
+conan install . -pr:a=conan\windows-msvc-17-shared-relwithdebinfo-x64.ini --output-folder=build --build=missing
 
 cmake -G "Visual Studio 17 2022" -A x64 -B build -S . -DCMAKE_TOOLCHAIN_FILE=build\conan_toolchain.cmake
